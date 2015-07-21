c27a560850dbc52544c68657e5dbcedf44177451
tomatell <tomatell@gmail.com>
2015-07-21 11:28:19 +0200

    [FIXED #99497450] Authentication failed warning on logout
    Fixed by resetting the local storage before displaying the login screen.

--- src/client/js/xpsui/controller/security-logout-ctrl.js
+++ src/client/js/xpsui/controller/security-logout-ctrl.js
@@ -4,11 +4,12 @@
 	angular.module('xpsui:controllers')
 	.controller(
 		'xpsui:SecurityLogoutCtrl',
-		[ '$scope', 'xpsui:SecurityService', '$location',
-				function($scope, SecurityService, $location) {
+		[ '$scope', 'xpsui:SecurityService', '$location', '$localStorage',
+				function($scope, SecurityService, $location, $localStorage) {
 					$scope.logout = function() {
 						SecurityService.getLogout().then(function() {
 							$scope.security.currentUser = undefined;
+							$localStorage.$reset();
 							$location.path('/login');
 						});
 					};
