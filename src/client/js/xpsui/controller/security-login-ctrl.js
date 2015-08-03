50a1d2d05bc4713a8e5718d7a851589fe92e3db4
tomatell <tomatell@gmail.com>
2015-08-03 14:26:57 +0200

    [FIXED #100306838] Security login control not sending rememberMe flag
    - the update to security-login-ctrl.js was not reflected from the previous
      manual update.
    - the update to security-service.js was not reflected from the previous
      manual update.
    - added missing security logout control update.

--- src/client/js/xpsui/controller/security-login-ctrl.js
+++ src/client/js/xpsui/controller/security-login-ctrl.js
@@ -8,41 +8,68 @@
 		'$location',
 		'xpsui:NotificationFactory', 
 		'xpsui:NavigationService' ,
-		function($scope, SecurityService, $rootScope, $location,notificationFactory, navigationService) {
+		'$cookies',
+		function($scope, SecurityService, $rootScope, $location,notificationFactory, navigationService, $cookies) {
 			// FIXME remove this in production
 			// $scope.user = 'johndoe';
 			// $scope.password = 'johndoe';
 			$scope.user = '';
 			$scope.password = '';
 
-			/**
-			 * Login button click
-			 */
-			$scope.login = function() {
-				SecurityService.getLogin($scope.user, $scope.password).success(function(user) {
-					if (user.systemCredentials.profiles.length>1){
-						$scope.profiles=user.systemCredentials.profiles;
-					}
-					else {
-						SecurityService.selectProfile(user.systemCredentials.profiles[0].id).success(function(){
-							SecurityService.getCurrentUser().success(function(data){
-							$rootScope.security.currentUser=data;
-							
-							if (!navigationService.back()) {
-								$location.path('/dashboard');
-							}
+			if($cookies.rememberMe) {
+				SecurityService.getLogin($scope.user, $scope.password, $cookies.rememberMe).success(function(user) {
+					if (user.systemCredentials.profiles.length > 1) {
+						$scope.profiles = user.systemCredentials.profiles;
+					} else {
+						SecurityService.selectProfile(user.systemCredentials.profiles[0].id).success(function() {
+							SecurityService.getCurrentUser().success(function(data) {
+								$rootScope.security.currentUser = data;
+								if (!navigationService.back()) {
+									$location.path('/personal-page');
+								}
 							});
 						});
 					}
 				}).error(function(err) {
-					if (err){
+					if (err) {
 						console.log(err);
 					}
 					delete $rootScope.security.currentUser;
-					var mes = {translationCode:'login.authentication.failed',time:5000};
+					var mes = {
+						translationCode : 'login.authentication.failed',
+						time : 5000
+					};
 					notificationFactory.error(mes);
 				});
-			};
+			} else {
+				/**
+				* Login button click
+				 */
+				$scope.login = function() {
+					SecurityService.getLogin($scope.user, $scope.password, $scope.rememberMe).success(function(user) {
+						$cookies.rememberMe = $scope.rememberMe;
+						if (user.systemCredentials.profiles.length > 1) {
+							$scope.profiles=user.systemCredentials.profiles;
+						} else {
+							SecurityService.selectProfile(user.systemCredentials.profiles[0].id).success(function(){
+								SecurityService.getCurrentUser().success(function(data){
+									$rootScope.security.currentUser=data;
+									if (!navigationService.back()) {
+										$location.path('/dashboard');
+									}
+								});
+							});
+						}
+					}).error(function(err) {
+						if (err){
+							console.log(err);
+						}
+						delete $rootScope.security.currentUser;
+						var mes = {translationCode:'login.authentication.failed',time:5000};
+						notificationFactory.error(mes);
+					});
+				}
+			}
 
 			$scope.selectProfile=function(){
 				if (!$scope.selectedProfile) return;
@@ -59,7 +86,7 @@
 			$scope.resetPassword = function() {
 				SecurityService.getResetPassword($scope.user);
 			};
-		} 
+		}
 	]);
 
 }(window.angular));
